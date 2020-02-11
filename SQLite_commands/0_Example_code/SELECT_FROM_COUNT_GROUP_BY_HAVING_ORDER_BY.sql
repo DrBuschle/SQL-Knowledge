b9e7b9f8f5751723acd9b05b4983e1cd2bc73577
DrBuschle <dr.buschle@hotmail.com>
2020-02-11 14:19:17 +0100

    Newest_updates

--- SQLite_commands/0_Example_code/SELECT_FROM_COUNT_GROUP_BY_HAVING_ORDER_BY.sql
+++ SQLite_commands/0_Example_code/SELECT_FROM_COUNT_GROUP_BY_HAVING_ORDER_BY.sql
@@ -1,6 +1,7 @@
 -- example HAVING
 
--- Get the tracks, which are on the same album, which has more than 10 tracks
+-- Get the tracks, which are on the same album, and sum up to more than 10 tracks
+-- and order them by from high to low
 
 SELECT COUNT(TrackID), AlbumID
 FROM Tracks

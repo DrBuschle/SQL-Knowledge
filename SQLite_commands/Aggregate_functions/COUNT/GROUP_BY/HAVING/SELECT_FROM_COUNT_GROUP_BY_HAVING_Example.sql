4f5c359359ac00c3d4ce092e00d20d52ab41ed5c
DrBuschle <dr.buschle@hotmail.com>
2020-02-11 14:03:16 +0100

    Newest_updates

--- SQLite_commands/Aggregate_functions/COUNT/GROUP_BY/HAVING/SELECT_FROM_COUNT_GROUP_BY_HAVING_Example.sql
+++ SQLite_commands/Aggregate_functions/COUNT/GROUP_BY/HAVING/SELECT_FROM_COUNT_GROUP_BY_HAVING_Example.sql
@@ -2,7 +2,7 @@
 
 -- Get the tracks, which are on the same album, which has more than 10 tracks
 
-SELECT COUNT(TrackID)
+SELECT COUNT(TrackID), AlbumID
 FROM Tracks
 GROUP BY AlbumID
 HAVING COUNT(TrackID) >= 10;

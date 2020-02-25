d395b41a45bbc0aa1901812d01fc271d47cc87d9
DrBuschle <dr.buschle@hotmail.com>
2020-02-25 13:19:36 +0100

    Newest_updates

--- SQLite_commands/Strings/SUBSTR/SELECT_SUBSTR.sql
+++ SQLite_commands/Strings/SUBSTR/SELECT_SUBSTR.sql
@@ -5,10 +5,18 @@
 
 -- get the characters from column 1 from your desired start to end position of the word
 SELECT Column_Name_1,
-	SUBSTR(Column_Name_1, string_postion_start, characters_to_returnreturn)
+	SUBSTR(Column_Name_1, string_postion_start, characters_to_return)
 FROM Table_Name_1;
 
 
+
 -- e.g
-SUBSTR(Column_Name_1, 2,3)		-- the word is Alexander
--- -> out put is lex
+SUBSTR(Column_Name_1, 2,3)		-- in case the the word is Hello, the output is:
+-- -> out put is ell
+
+
+-- if you don't give the character to return, everything behind the first letter will be returned
+SUBSTR(Column_Name_1, 2)		-- in case the the word is Hello, the output is:
+-- -> out put is ello
+
+
